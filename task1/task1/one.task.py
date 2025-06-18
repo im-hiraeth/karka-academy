1e61f94fc833db6ea52faffcdfeffc578f8dfb88
veena.R <veena030705@gmail.com>
2025-06-18 05:11:20 -0700

    Add files via upload

---
+++ task1/task1/one.task.py
@@ -0,0 +1,84 @@
+#1
+# print("Hello,world!!")
+
+#2
+# print("myself veena Rajakumar")
+
+#3
+# print("myself veena Rajakumar")
+# print("I am 21")
+
+#4
+# name = "veena"        
+# is_student = True     
+# height = 5.6          
+# age = 20             
+# status = not is_student          
+# print("hello",name)
+# print("Is not a student:", status)
+# print("age:,age")
+# print("height",height)
+
+#5
+# name = "veena"                 
+# age=20
+# infomessage = "My name is " + name + " and I am " + str(age) + " years old."
+# print(infomessage)
+
+#6
+# name = "veena"
+# age = 20 
+# print("name:",name)
+# print("age:",age)
+
+#7
+# a=3
+# b=3
+# addition=a+b
+# sub=a-b
+# multiplication=a*b
+# division=a/b
+# print("addition",addition)
+# print("subraction",sub)
+# print("multiplication",multiplication)
+# print("division",division)
+
+#8
+# print("*")
+# print("* *")
+# print("* * *")
+# print("* * * *")
+# print("* * * * *")
+
+#9
+# name = "veena"               
+# age = 20   
+# isstudent = True             
+# print(name)
+# print(age)
+# print(isstudent)
+# name="deepak"
+# age=age+1
+# status=not isstudent
+# print(name)
+# print(age)
+# print(status)
+
+
+
+ #10
+# name="veena"
+# age=21
+# dob="10-3-2005"
+# height=5.4
+# weight=45 
+# degree="BE"
+# Gender="female"
+# print("personal details")
+# print("name:",name)
+# print("age:",age)
+# print("dob:",dob)
+# print("height:",height)
+# print("weight:",weight)
+# print("degree:",degree)
+# print("gender:",Gender)
