a8d284dcc8517634b8e61f3d8d3db73c6f4cfce1
veena.R <veena030705@gmail.com>
2025-06-20 05:23:03 -0700

    Add files via upload

---
+++ task2/tsk2/task3.py
@@ -0,0 +1,32 @@
+#1
+# a=55
+# b=33
+# print(a+b)
+
+#2
+# a=10
+# a+=a
+# print(a)
+# a-=a
+# print(a)
+
+#3
+# a=30
+# a+=20
+# print(a)
+# a-=20
+# print(a)
+# a*=20
+# print(a)
+# a/=20
+# print(a)
+ 
+
+#4
+# print(20!=50)
+# print(30==50)
+# print(66<=50)
+# print(77>=39)
+# print(30<60)
+# print(40>30)
+
