83f72409df3fb1d25596fc96e410bae5b5128cb5
veena.R <veena030705@gmail.com>
2025-06-25 04:24:14 -0700

    Add files via upload

---
+++ task/taskthree.py
@@ -0,0 +1,53 @@
+#1
+# def sayhello(name):
+#     print(f"hello,{name}!")
+# sayhello("deepak")
+
+# #2
+# a=2
+# b=3
+# def add():
+#     print(a+b)
+# add()    
+
+#3
+def multiply(x,y):
+    return x*y
+print(multiply(3,4))
+print(multiply(5,4))
+print(multiply(7,10))
+
+    
+
+
+
+#4
+# a=5
+# b=20
+# def mul():
+#     print(a*b)
+# mul()    
+
+#5
+# a=60
+# b=40
+# def div():
+#      print(a/b)
+# div()
+
+
+
+#7
+# def square(n):
+#     sq=n*n
+#     print(sq)
+# square(4)
+
+#6
+# def factorial(n):
+#     if n==0:
+#         return 1
+#     ans=n*factorial(n-1)
+#     return ans
+# print (factorial(5))
+# print(factorial(2))
